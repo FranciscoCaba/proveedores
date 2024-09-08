cf400a39ac7d7d3aa3c9093345d1aa14f3264e07
FranciscoCaba <francorv2@fpuna.edu.py>
2024-09-08 18:58:27 -0400

    Archivos iniciales agregados para trabajo

---
+++ src/app/jaulas/jaulas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jaulas',
+  standalone: true,
+  imports: [],
+  templateUrl: './jaulas.component.html',
+  styleUrl: './jaulas.component.css'
+})
+export class JaulasComponent {
+
+}
